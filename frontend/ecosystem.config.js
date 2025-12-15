4a26db27154205b264baad87cdd7d7e45fdbcfa5
Anton Kuzmenko <m1800555@yandex.ru>
2025-12-15 21:15:56 +0300

    Update ecosystem

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './.env.deploy' });
 
 const {
-  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPO,
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPO, PRIVATE_KEY_PATH,
 } = process.env;
 
 module.exports = {
@@ -13,6 +13,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
+      key: PRIVATE_KEY_PATH,
       'post-deploy': 'cd frontend && npm i && npm run build',
     },
   },
