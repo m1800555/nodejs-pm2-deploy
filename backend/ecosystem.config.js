ae1e0a9055512d59bd218315de5023ed33f9e544
Anton Kuzmenko <m1800555@yandex.ru>
2025-12-15 14:35:54 +0300

    Add domain

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -7,7 +7,7 @@ const {
 
 module.exports = {
   apps: [{
-    name: "mesto",
+    name: "mesto_backend",
     script: "./dist/app.js"
   }],
   deploy: {
@@ -17,7 +17,6 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
-      'pre-deploy-local': `scp ./.env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
       'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
       'post-deploy': 'cd backend && npm i && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
     },
