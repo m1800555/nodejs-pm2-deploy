894e223b279f913c6d3686f08f2ce9f05edce8c1
Anton Kuzmenko <m1800555@yandex.ru>
2025-12-16 16:38:30 +0300

    Add use deployEnv.sh

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -18,7 +18,7 @@ module.exports = {
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
       key: PRIVATE_KEY_PATH,
-      'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'pre-deploy-local': `bash scripts/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST} ${DEPLOY_PATH}`,
       'post-deploy': 'cd backend && npm i && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
     },
   },
